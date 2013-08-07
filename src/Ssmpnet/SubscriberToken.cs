c26d4701b04c5315ad12912d89839b5ce646776d
Ziya Suzen <ziya@suzen.net>
2013-08-07 16:42:34 +0100

    Subscriber internal queue implementation
    Implemented an internal blocking queue for the subscriber. This will be
    the connection between the subscriber's receiver callback and the socket
    i/o threads. It should help in case subscriber has some heavy processing
    inside the callback.

--- src/Ssmpnet/SubscriberToken.cs
+++ src/Ssmpnet/SubscriberToken.cs
@@ -1,27 +1,46 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 
 namespace Ssmpnet
 {
     internal class SubscriberToken
     {
+        private readonly PacketProtocol _packetProtocol;
+        private readonly BlockingCollection<byte[]> _q = new BlockingCollection<byte[]>(1000);
+        
         internal Socket Socket;
+        
         internal IPEndPoint EndPoint;
-        internal PacketProtocol PacketProtocol;
 
-        internal SubscriberToken(Socket socket)
-        {
-            Socket = socket;
-        }
+        internal Action<byte[]> Receiver { get; set; }
+        
+        internal Action Connected;
 
-        internal SubscriberToken(Socket socket, IPEndPoint endPoint)
+        internal SubscriberToken(Socket socket, IPEndPoint endPoint, Action<byte[]> receiver)
         {
             Socket = socket;
             EndPoint = endPoint;
+            Receiver = receiver;
+            _packetProtocol = new PacketProtocol { MessageArrived = receiver };
+            ThreadPool.QueueUserWorkItem(ConsumeQueue);
         }
 
-        internal Action<byte[]> Receiver { get; set; }
-        internal Action Connected;
+        internal void Enqueue(byte[] buffer, int offset, int count)
+        {
+            var bytes = new byte[count];
+            Buffer.BlockCopy(buffer, offset, bytes, 0, count);
+            _q.Add(bytes);
+        }
+
+        private void ConsumeQueue(object state)
+        {
+            foreach (var buffer in _q.GetConsumingEnumerable())
+            {
+                _packetProtocol.DataReceived(buffer);
+            }
+        }
     }
 }
